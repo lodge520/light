77269b8c1089548ea54188da6bdf5f90647254fd
lodge520 <13724127262@163.com>
2026-04-14 10:39:57 +0800

    后端功能更新

--- smart-light-backend/smart-light-backend/src/main/java/com/genius/smartlight/common/ServiceException.java
+++
@@ -1,8 +0,0 @@
-package com.genius.smartlight.common;
-
-public class ServiceException extends RuntimeException {
-
-    public ServiceException(String message) {
-        super(message);
-    }
-}
--- smart-light-backend/smart-light-backend/src/main/java/com/genius/smartlight/dal/mysql/DeviceMapper.java
+++
@@ -1,9 +0,0 @@
-package com.genius.smartlight.dal.mysql;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.genius.smartlight.dal.dataobject.DeviceDO;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface DeviceMapper extends BaseMapper<DeviceDO> {
-}
